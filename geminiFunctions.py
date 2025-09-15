5a1ceafafdf01b15e7d07a2bd8b1c0987b8d1499
Pierce Boyack <97429249+PierceBoyack@users.noreply.github.com>
2025-09-15 00:00:53 -0400

    Added Gemini function calls

---
+++ geminiFunctions.py
@@ -0,0 +1,114 @@
+import os
+from typing import Optional, Tuple
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types as genaiTypes
+from google.genai import errors as genaiErrors
+from google.genai.chats import Chat
+# google.generativeai is deprecated
+# Use google.genai
+
+# Google Cloud requires authentication for Vertex AI using non-express APIs.
+# You'll have to set up a Workload Identity Federation or Application Default Credentials.
+
+
+# System instructions are processed before the model takes in a prompt
+# Use it to edit persona, contextual information, and formatting instructions
+systemInstruction = ["You are a friendly and helpful assistant.", "You are a talking frog.",
+                     "Output responses in simple text. Do not use Markdown or YAML.",
+                     "Ensure your answers are concise, unless the user requests a deeper explanation.",
+                     "You are permitted to make jokes occasionally."]
+
+load_dotenv()
+geminiKey = os.getenv("GEMINI_API_KEY")
+gcpProject = os.getenv("GOOGLE_CLOUD_PROJECT")
+gcpLocation = os.getenv("GOOGLE_CLOUD_LOCATION")
+client = genai.Client(vertexai=True, project=gcpProject, location=gcpLocation)
+
+# Gemini can do more than just text depending on the model,
+# but we want to specify our model for only using text based requests.
+# For Gemini models, a token is equivalent to about 4 characters. 
+# 100 tokens is equal to about 60-80 English words.
+def countInputPromptTokens(prompt: str, model: str="gemini-2.5-flash") -> int:
+  """
+  Count the total tokens used for an input prompt using the specified model
+  Args:
+    prompt (str): The string to count tokens for
+    model (str): The gemini model resource name
+  Returns:
+    totalInputTokens (int): Total token cost of the given prompt for the given model
+  """
+  totalTokens = client.models.count_tokens(
+    model=model, contents=prompt
+  )
+  return totalTokens.total_tokens
+
+def totalRequestTokens(response: genaiTypes.GenerateContentResponse) -> int:
+  """
+  Return the total tokens spent on a given request, from input, to thinking budget, to output
+  Returns:
+    totalResponseTokens (int): Total tokens spent on sending and receiving a response
+  """
+  # print(response.usage_metadata)
+  return response.usage_metadata.total_token_count
+
+def geminiTextRequest(prompt: str, model: str="gemini-2.5-flash") -> Optional[str]:
+  """
+  Make a prompt request to the specified model
+  Args:
+    prompt (str): The string to be responded to by gemini
+    model (str): The gemini model resource name
+  Returns:
+    response.text (str): The gemini text response
+    - returns None on error
+  """
+  try:
+    response = client.models.generate_content(
+      model=model,
+      contents=prompt,
+      config=genaiTypes.GenerateContentConfig(
+          thinking_config=genaiTypes.ThinkingConfig(thinking_budget=-1), # 0 Disables thinking, -1 Dynamic thinking
+          system_instruction=systemInstruction
+      )
+    )
+    print(f"Gemini says: {response.text}\n")
+    totalTokenCost = totalRequestTokens(response)
+    print(f"Total Token Cost: {totalTokenCost}\n")
+    return response.text
+  except genaiErrors.APIError as e:
+    print(e.code)
+    print(e.message)
+    return None
+
+def geminiChatRequest(prompt: str, chat: Optional[Chat] = None, model: str="gemini-2.5-flash") -> Tuple[Optional[str], Optional[Chat]]:
+  """
+  Start or continue a gemini chat session
+  Args:
+    prompt: The string to be responded to by gemini
+    chat: The ongoing chat conversation. If None, a new chat is initialized
+    model: The gemini model resource name
+  Returns:
+    (response.text, chat) (Tuple[str, Chat]):
+    - response.text (str): The gemini text response
+    - chat (Chat): The new or ongoing chat session
+    - returns (None, None) on error
+  """
+  if chat == None:
+    chat = client.chats.create(
+      model=model,
+      config=genaiTypes.GenerateContentConfig(
+          thinking_config=genaiTypes.ThinkingConfig(thinking_budget=-1), # 0 Disables thinking, -1 Dynamic thinking
+          system_instruction=systemInstruction,
+          max_output_tokens=20000
+        )
+    )
+  try:
+    response = chat.send_message(prompt)
+    print(f"Gemini says: {response.text}\n")
+    totalTokenCost = totalRequestTokens(response)
+    print(f"Total Token Cost: {totalTokenCost}\n")
+    return response.text, chat
+  except genaiErrors.APIError as e:
+    print(e.code)
+    print(e.message)
+    return None, None
--- voiceCommands.py
+++ voiceCommands.py
@@ -2,60 +2,90 @@ import os
 from dotenv import load_dotenv
 import pvporcupine
 from pvrecorder import PvRecorder
-import sounddevice
 import speech_recognition as sr
 import pyaudio
 import psutil
+from geminiFunctions import geminiChatRequest
 
 load_dotenv()
 
-picokey = os.getenv("PICOKEY")
+picokey = os.getenv("PICO_KEY")
 porcupine = pvporcupine.create(
   access_key=picokey,
   keywords=["picovoice"]
-  
 )
 
-recorder = PvRecorder(frame_length=porcupine.frame_length)
+recorder = PvRecorder(device_index=-1, frame_length=porcupine.frame_length)
 recorder.start()
 
-def testCommand():
-  print("Listening...")
-  print("Done")
-
 def processAudio():
+  """
+  Use the initialized PvRecorder to process the current audio window to detect the porcupine wake word
+  Returns:
+    processResult (int): the index of the detected porcupine keyword or -1 if none were detected
+  """
   pcm = recorder.read()
   return porcupine.process(pcm)
 
 def closeRecorder():
+  """Stops the initiallized PvRecorder from recording audio and releases any resources used by the Pvrecorder"""
   recorder.stop()
   recorder.delete()
   porcupine.delete()
 
 
 def speechToText():
+  """
+  Take in speech from the user's default mic and convert it to text
+  Returns:
+    text (str): User's speech converted to a text str or None on error
+  """
   recognizer = sr.Recognizer()
   with sr.Microphone() as source:
     audio = recognizer.listen(source)
     try:
       text = recognizer.recognize_google(audio, language="en-US")
-      print(f"You said: {text}")
+      print(f"You said: {str(text)}")
+      return str(text)
     except Exception as e:
       print(e)
       print("Please try again")
+      return None
 
 def get_ram_usage():
-  """Returns the current RAM usage of the process in MiB."""
+  """Returns the current RAM usage of the process in MiB"""
   process = psutil.Process(os.getpid())
   mem_info = process.memory_info()
   return mem_info.rss / 1024 ** 2 # Convert bytes to MiB
 
 if __name__ == "__main__":
+  geminiChat = None
   while True:
     detectWake = processAudio()
     if detectWake != -1:
       recorder.stop()
       print("Listening...")
-      speechToText()
+      userPrompt = speechToText()
+      if userPrompt == None:
+        # alert user
+        print("\nError has occured")
+        print("Prompt Ended")
+        recorder.start()
+        continue
+      if userPrompt == "reset chat":
+        geminiChat = None
+        print("\nChat Reset")
+        print("Prompt Ended")
+        recorder.start()
+        continue
+
+      geminiResponse, geminiChat = geminiChatRequest(
+          prompt=userPrompt,
+          chat=geminiChat,
+          model="gemini-2.5-flash"
+        )      
+      if geminiResponse == None:
+        print("Error has occured")        
+        
+      print("\nPrompt Ended")
       recorder.start()
-      print("Prompt Ended")
